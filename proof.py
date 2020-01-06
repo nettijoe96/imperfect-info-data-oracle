dbe1b772ea95c1d541fe1de17c6f6a1486e2d9a7
Joe Netti <nettijoe@gmail.com>
2020-01-05 20:37:28 -0500

    far more efficient calculation using binomial formulate rather than enumeration

--- proof.py
+++ proof.py
@@ -1,10 +1,13 @@
 from matplotlib import pyplot as plt
 import bayes
 from itertools import combinations
+import operator as op
+from functools import reduce
 
-m = 30
-n = 8
-pi = .5
+
+m = 1000
+n = 30
+pi = .78
 
 priorsCherryProb100 = [1/100 * i for i in range(0, 101)]    #percent of cherrys from 0% to 100%
 priors100 = [1/len(priorsCherryProb100) for i in range(0, len(priorsCherryProb100))] #equal starting priors
@@ -16,38 +19,46 @@ def main():
     piCountDict = {}
     totalMatches = 0
     combProb = {}
+    numOfCherriesNeeded = None
+    for i in range(0, n+1):   #number of cherry's to get prob
+        priors = [p for p in initalPriors]
+        prob = round(getPi(i, n-i, priors, priorsCherryProb), 2)
+        print(prob)
+        if prob == pi:
+            numOfCherriesNeeded = i
+            break
+    if numOfCherriesNeeded is None:
+        print("choose different pi")
+        return
+
     for i in range(0, len(priorsCherryProb)):
         print("prior ", i, "is true")
         count = 0
-        combs = getCombinations(data[i])
-        for comb in combs:
-            combKey = getCombKey(comb)
-            if combKey in combProb:
-                prob = combProb[combKey]
-            else: 
-                priors = [p for p in initalPriors]
-                prob = round(getPi(comb, priors, priorsCherryProb),1)
-                combProb[combKey] = prob
-                print(prob)
-            if prob == pi:              # TODO: maybe we should check within a range
-                totalMatches += 1 
-                count += 1
-        key = str(priorsCherryProb[i])
-        piCountDict[key] = count
-        combs = None
-         
+        ds = data[i]
+        cherry = 0
+        lime = 0
+        for d in ds:
+            if d == "cherry":
+                cherry += 1
+            else:
+                lime += 1
+        percentCherry = cherry/len(ds)
+        percentLime = lime/len(ds)
+        
+        #prob of getting number of cherries needed
+        combNum = ncr(m, n)
+        binomial = round(combNum * (percentCherry**numOfCherriesNeeded) * (percentLime**(n-numOfCherriesNeeded)))
+        piCountDict[str(priorsCherryProb[i])] = binomial
+        totalMatches += binomial 
+
     graphPiCount(priorsCherryProb, piCountDict, totalMatches)
  
 
-def getCombKey(comb):
-    cherry = 0
-    lime = 0
-    for ele in comb:
-        if ele == "cherry":
-            cherry += 1
-        else:
-            lime += 1
-    return str((cherry, lime))
+def ncr(a, b):
+    r = min(b, a-b)
+    numer = reduce(op.mul, range(a, a-b, -1), 1)
+    denom = reduce(op.mul, range(1, r+1), 1)
+    return numer / denom
 
 
 def graphPiCount(priorsCherryProb, piCountDict, totalMatches):
@@ -64,21 +75,19 @@ def generateData(priorsCherryProb):
     return data
 
 
-def getCombinations(data):
-    return list(combinations(data, n))
-
-
-def getPi(data, initalPriors, priorsCherryProb):
+def getPi(cherries, limes, initalPriors, priorsCherryProb):
     priors = initalPriors
-    for i in range(0, len(data)):
-        d = data[i]
+    for i in range(0, cherries):
         priorsUnscaled = []
-        if d == "cherry":
-             for j in range(0, len(priors)):
-                priorsUnscaled += [priorsCherryProb[j] * priors[j]]
-        else:
-            for j in range(0, len(priors)):
-                priorsUnscaled += [(1-priorsCherryProb[j]) * priors[j]]
+        for j in range(0, len(priors)):
+            priorsUnscaled += [priorsCherryProb[j] * priors[j]]
+        s = sum(priorsUnscaled)
+        for j in range(0, len(priors)):
+            priors[j] = priorsUnscaled[j] / s 
+    for i in range(0, limes):
+        priorsUnscaled = []
+        for j in range(0, len(priors)):
+            priorsUnscaled += [(1-priorsCherryProb[j]) * priors[j]]
         s = sum(priorsUnscaled)
         for j in range(0, len(priors)):
             priors[j] = priorsUnscaled[j] / s 
@@ -87,8 +96,8 @@ def getPi(data, initalPriors, priorsCherryProb):
     for j in range(0, len(priors)):
         prob += priorsCherryProb[j] * priors[j]
 
-
     return prob
+
 
 main()
 
